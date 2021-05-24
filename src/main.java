98c8fcbef5d3e468c6e11792385e7521fce9f095
Matthew <matthewhwallace17@gmail.com>
2021-05-24 19:35:49 +0100

    Buy drink fuctionality added

--- src/main.java
+++ src/main.java
@@ -8,11 +8,12 @@ public class main {
 		Scanner sc = new Scanner(System.in);
 		
 		while(run) {
-			String toPrint = "--Welcome to our vending machine!-- \n Please select an option below";
+			String toPrint = "\n--Welcome to our vending machine!-- \n Please select an option below";
 			toPrint+= "\n1 - Print drinks list";
 			toPrint+= "\n2 - Add Drinks";
 			toPrint+= "\n3 - Print change available";
 			toPrint+= "\n4 - Add change";
+			toPrint+= "\n5 - Buy Drink";
 			toPrint+="\nx - quit";
 			System.out.println(toPrint);
 			
@@ -29,6 +30,19 @@ public class main {
 				break;
 			case "4":
 				System.out.println(v.addChange());
+				break;
+			case "5":
+				String printDrink ="Which drink would you like?";
+				if(v.productsAvailable.size()==0) {
+					printDrink ="No Drinks Available";
+				}
+				for(int i = 0 ;i<v.productsAvailable.size();i++) {
+					printDrink+= "\n"+(i + 1) +" - " + v.productsAvailable.get(i).name;
+				}
+				System.out.println(printDrink);
+				int choice = Integer.parseInt(sc.nextLine().trim());
+				v.buyDrink(choice-1);
+				
 				break;
 			case "x":
 				System.out.println("Goodbye!");
--- src/vendingMachine.java
+++ src/vendingMachine.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class vendingMachine {
 	ArrayList<product> productsAvailable = new ArrayList<product>();
-	int numOf1;
-	int numOf5;
-	int numOf10;
-	int numOf25;
-	int sumCash = numOf1 + 5*numOf5 + 10*numOf10 + 25*numOf25;
+	static int numOf1;
+	static int numOf5;
+	static int numOf10;
+	static int numOf25;
+	static int sumCash = numOf1 + 5*numOf5 + 10*numOf10 + 25*numOf25;
 	
 	
 	public vendingMachine() {	
@@ -16,7 +17,121 @@ public class vendingMachine {
 		numOf25 = 0;
 	}
 	
+	public void buyDrink(int choice) {
+		if (choice<productsAvailable.size()) {
+			if (productsAvailable.get(choice).quantity == 0) {
+				System.out.println(productsAvailable.get(choice).name + " is unavailable");
+			}else {
+				System.out.println("Please enter change");
+				boolean correctChange = vendingMachine.enterChange(productsAvailable.get(choice).price);
+				if(correctChange) {
+					productsAvailable.get(choice).quantity--;
+					System.out.println("\nEnjoy Your " + productsAvailable.get(choice).name + "!\n");
+				}
+			}
+			
+		}
+		
+	}
+	
+	private static boolean enterChange(int cost) {
+		Scanner sc = new Scanner(System.in);
+		boolean returnBool = true;
+		int total = 0;
+		int count1 = 0;
+		int count5 = 0;
+		int count10 = 0;
+		int count25 = 0;
+		
+		while(total<cost) {
+			
+		System.out.println("Total due: " + (cost-total) + "\nTotal paid: " + total + "\nPlease enter a coin" );
+		int coin = sc.nextInt();
+		switch(coin) {
+
+		case 1 :
+			count1++;
+			total = total + coin;
+			break;
+		case 5:
+			count5++;
+			total = total + coin;
+			break;
+		case 10:
+			count10++;
+			total = total + coin;
+			break;
+		case 25:
+			count25++;
+			total = total + coin;
+			break;		
+			default :
+				System.out.println("\nInvalid coin. \nWould you like to cancel the transaction? y/n\n");
+				String quit = sc.next();
+				if(quit.equals("y")) {
+					return false;
+				}
+		}
+		
+		
+		}
+		System.out.println("\nAre you sure you want to buy a drink? y/n" );
+		String quit = sc.next();
+		if(quit.equals("n")) {
+			System.out.println("You have been refunded");
+			return false;
+		}else {
+			numOf1 = numOf1 + count1;
+			numOf5 = numOf5 + count5;
+			numOf10 = numOf10 + count10;
+			numOf25 = numOf25 + count25;
+			int changeDue = total - cost;
+			count1 = 0;
+			count5 = 0;
+			count10 = 0;
+			count25 = 0;
+			while(changeDue>0) {
+				if(changeDue>=25) {
+					count25++;
+					changeDue = changeDue - 25;
+				}else if(changeDue>=10) {
+					count10++;
+					changeDue = changeDue - 10;
+				}else if(changeDue>=5) {
+					count5++;
+					changeDue = changeDue - 5;
+				}else if(changeDue>=1) {
+					count1++;
+					changeDue = changeDue - 1;
+				}
+				
+			}
+			String yourChange="Your change is ";
+			for(int i = 1;i<=count25;i++) {
+				numOf25--;
+				yourChange += "25 ";
+			}
+			for(int i = 1;i<=count10;i++) {
+				numOf10--;
+				yourChange += "10 ";
+			}
+			for(int i = 1;i<=count5;i++) {
+				numOf5--;
+				yourChange += "5 ";
+			}
+			for(int i = 1;i<=count1;i++) {
+				numOf1--;
+				yourChange += "1 ";
+			}
+			System.out.println(yourChange);
+		}
+		sumCash = numOf1 + 5*numOf5 + 10*numOf10 + 25*numOf25;
+		return returnBool;
+	}
+
 	public String addDrinks() {
+		ArrayList<product> blank = new ArrayList<product>();
+		productsAvailable = blank;
 		String returnString = "Drinks added";
 		product coke = new product("Coke", 25, 1);
 		product pepsi = new product("Pepsi", 35, 3);
@@ -61,7 +176,7 @@ public class vendingMachine {
 		String toPrint = "";
 		if(productsAvailable.size()>0) {
 			for(int i =0; i<productsAvailable.size();i++) {
-				toPrint+= "There ";
+				toPrint+= "\nThere ";
 				if(productsAvailable.get(i).quantity==1) {
 					toPrint+= "is";
 				}else {
